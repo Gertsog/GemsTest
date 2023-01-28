f307bc4270fe76b45738f80610a9f46e217819e8
Gertsog <asgertsog@gmail.com>
2023-01-28 22:35:59 +0600

    Add commentaries

--- ImageMerging/Program.cs
+++ ImageMerging/Program.cs
@@ -10,28 +10,30 @@ namespace ImageMerging
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Введите путь к папке с изображениями:");
-            var inputText = Console.ReadLine();
             try
             {
-                var path = Path.GetFullPath(inputText);
+                if (args.Length == 0)
+                    throw new("Необходимо указать путь к папке с изображениями");
+
+                var path = args[0].Trim();
                 var filePaths = GetFileArray(path);
                 var imagePaths = FilterImageArray(filePaths);
 
                 using var resultImage = CreateImage(imagePaths);
 
                 var resultName = GenerateResultName();
-                var outPath = Path.Combine(path, resultName);
+                var outPath = Path.Combine(path, resultName); //Возможно, стоило создать отдельную папку для финальных изображений
 
                 SaveImage(resultImage, outPath);
                 Console.WriteLine($"Создано изображение {outPath}");
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
             }
         }
 
+        //Получение списка файлов из директории
         private static string[] GetFileArray(string path)
         {
             if (!Directory.Exists(path))
@@ -40,6 +42,7 @@ namespace ImageMerging
             return Directory.GetFiles(path);
         }
 
+        //Получение из списка файлов изображений формата Image.row.col.jpg
         private static string[] FilterImageArray(string[] images)
         {
             var correctFileName = new Regex(@".*\\Image\.\d+\.\d+\.(jpe?g|JPE?G)$");
@@ -47,41 +50,35 @@ namespace ImageMerging
             return result;
         }
 
-        private static SKImage CreateWhiteImage(int width, int height)
-        {
-            using var tempSurface = SKSurface.Create(new SKImageInfo(width, height));
-            var canvas = tempSurface.Canvas;
-            canvas.Clear(SKColors.White);
-
-            return tempSurface.Snapshot();
-        }
-
-        private static string GenerateResultName()
-        {
-            return "Result" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
-        }
-
-        private static void SaveImage(SKImage image, string path)
-        {
-            using SKData encoded = image.Encode(SKEncodedImageFormat.Jpeg, 100);
-            using Stream outFile = File.OpenWrite(path);
-            encoded.SaveTo(outFile);
-        }
-
+        //Создание финального изображения
         private static SKImage CreateImage(string[] imagePaths)
         {
+            //Если в папке нет подходящих изображений, создаётся белая картинка
             if (imagePaths.Length == 0)
-               return CreateWhiteImage(DefaultImageWidth, DefaultImageHeight);
+                return CreateWhiteImage(DefaultImageWidth, DefaultImageHeight);
 
+            //Если в папке одно подходящее изображение, создаётся его копия
             if (imagePaths.Length == 1)
             {
                 using SKBitmap bitmap = SKBitmap.Decode(imagePaths[0]);
                 return SKImage.FromBitmap(bitmap);
             }
 
+            //Иначе катинки объединяются
             return MergeImages(imagePaths);
         }
 
+        //Создание белого изображения
+        private static SKImage CreateWhiteImage(int width, int height)
+        {
+            using var tempSurface = SKSurface.Create(new SKImageInfo(width, height));
+            using var canvas = tempSurface.Canvas;
+            canvas.Clear(SKColors.White);
+
+            return tempSurface.Snapshot();
+        }
+
+        //Объединение изображений
         private static SKImage MergeImages(string[] imagePaths)
         {
             var imageIndexes = new Dictionary<string, (int, int)>();
@@ -100,16 +97,17 @@ namespace ImageMerging
             var maxRowIndex = rowIndexes.Max();
             var maxColIndex = colIndexes.Max();
 
-            var rowLength = 1 + maxRowIndex - minRowIndex;
+            //Расчёт количества картинок в каждом ряду и столбце
+            var rowLength = 1 + maxRowIndex - minRowIndex; //+1 т.к. по заданию картинки начинаются минимум с 1
             var colLength = 1 + maxColIndex - minColIndex;
 
+            //Расчёт размеров финального изображения при учёте, что у всех картинок одинаковые размеры
             var imageSize = GetImageSize(imagePaths[0]);
             var resultWidth = rowLength * imageSize.Item1;
             var resultHeigth = colLength * imageSize.Item2;
 
             using var surface = SKSurface.Create(new SKImageInfo(resultWidth, resultHeigth));
             using var canvas = surface.Canvas;
-
             canvas.Clear(SKColors.White);
             foreach (var image in imageIndexes)
             {
@@ -118,23 +116,18 @@ namespace ImageMerging
                 var imageRowIndex = image.Value.Item1;
                 var imageColIndex = image.Value.Item2;
 
-                using var bitmap = SKBitmap.Decode(image.Key);
-
-
+                //Расчёт отступов от краёв финального изображения в пикселях
                 var offsetX = (imageRowIndex - minRowIndex) * imageWidth;
                 var offsetY = (imageColIndex - minColIndex) * imageHeight;
+
+                using var bitmap = SKBitmap.Decode(image.Key);
                 canvas.DrawBitmap(bitmap, SKRect.Create(offsetX, offsetY, imageWidth, imageHeight));
             }
 
             return surface.Snapshot();
         }
 
-        private static(int, int) GetImageSize(string imagePath)
-        {
-            using var bitmap = SKBitmap.Decode(imagePath);
-            return (bitmap.Width, bitmap.Height);
-        }
-
+        //Получение индексов из названия файла
         private static (int, int) GetImageIndexes(string imagePath)
         {
             var fileName = Path.GetFileName(imagePath);
@@ -143,5 +136,27 @@ namespace ImageMerging
             var rowIndex = int.Parse(parts[2]);
             return (rowIndex, colIndex);
         }
+
+        //Получение размеров изображения
+        private static (int, int) GetImageSize(string imagePath)
+        {
+            using var bitmap = SKBitmap.Decode(imagePath);
+            return (bitmap.Width, bitmap.Height);
+        }
+
+        //Генерация названия финального изображения
+        private static string GenerateResultName()
+        {
+            return "Result" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
+        }
+
+        //Сохранение изображения
+        private static void SaveImage(SKImage image, string path)
+        {
+            using SKData encoded = image.Encode(SKEncodedImageFormat.Jpeg, 100);
+            using Stream outFile = File.OpenWrite(path);
+            encoded.SaveTo(outFile);
+        }
+        
     }
 }
